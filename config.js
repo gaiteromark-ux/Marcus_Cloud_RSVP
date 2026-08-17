420a0e124dab2819a6dadba2ac9f18521832d4e3
gaiteromark-ux <gaiteromark@gmail.com>
2026-08-17 15:28:54 +0800

    config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 */
 const APP_CONFIG = {
   // Replace this after deploying Code.gs as a Web App.
-  APPS_SCRIPT_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwyqbaWppPGo-c9WCbGTppHIMRg2jkafAx--Wh4YdkplrNiR8rppx8IQF9Cnuvim2B5IA/exec",
+  APPS_SCRIPT_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzPkF3A-OFU765mICjvzKwkZByyqfzGKOCaNNPkt0IKiO861-FuuDftJ4ecKAU04uV_xA/exec",
 
   childName: "Marcus Cloud Gaitero",
   eventDateISO: "2026-09-29T11:00:00+08:00",
