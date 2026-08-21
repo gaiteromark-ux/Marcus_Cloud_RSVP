5ea69cde60c16750a29d8f4ce185213b2f9da784
gaiteromark-ux <gaiteromark@gmail.com>
2026-08-21 11:14:10 +0800

    config.js

--- config.js
+++ config.js
@@ -9,28 +9,48 @@ const APP_CONFIG = {
 
   // Google Apps Script Web App
   APPS_SCRIPT_WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbyKYTZoPJqJselnj55UAVkv05heA0MQRrp9IeDv9HbOR5IES41c81nTqk7QC8HbKT2I3Q/exec",
+    "https://script.google.com/macros/s/AKfycbzPkF3A-OFU765mICjvzKwkZByyqfzGKOCaNNPkt0IKiO861-FuuDftJ4ecKAU04uV_xA/exec",
 
   // Child
-  childName:
-    "Marcus Cloud Gaitero",
+  childName: "Marcus Cloud Gaitero",
 
   // Event
-  eventDateISO:
-    "2026-09-29T11:00:00+08:00",
+  eventDateISO: "2026-09-29T11:00:00+08:00",
 
   // Organizer
-  organizerPhone:
-    "+639647544914",
+  organizerPhone: "+639647544914",
 
-  // Gift / Registry
-  //
-  // Leave this empty if you don't have an online
-  // gift/registry page yet.
-  //
-  // Example:
-  // giftRegistryUrl: "https://example.com/registry"
-  //
-  giftRegistryUrl: ""
+  // Online registry link
+  // Put your Shopee/Lazada/Amazon/etc. registry link here.
+  giftRegistryUrl: "",
+
+  // Gift list
+  giftList: [
+    {
+      icon: "💌",
+      name: "Cash Gift",
+      description: "For Marcus's future needs and little dreams."
+    },
+    {
+      icon: "👕",
+      name: "Baby Clothes",
+      description: "Comfortable clothes for Marcus."
+    },
+    {
+      icon: "🧸",
+      name: "Toys & Books",
+      description: "Age-appropriate toys, books, and learning materials."
+    },
+    {
+      icon: "🍼",
+      name: "Baby Essentials",
+      description: "Useful everyday baby essentials."
+    },
+    {
+      icon: "🧴",
+      name: "Baby Care",
+      description: "Baby care and hygiene products."
+    }
+  ]
 
 };
